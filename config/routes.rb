a8bdd637009491959ddf122e660f4fa3b399529b
Jim Publicover <jameslpublicover@gmail.com>
2019-05-20 18:16:39 -0400

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'panels/index'
+  devise_for :users
   # get 'dashboards/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
